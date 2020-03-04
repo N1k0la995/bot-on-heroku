1fe36645c98e96fd4c53f650afd9814350b86b59
N1k0la995 <46502036+N1k0la995@users.noreply.github.com>
2020-03-04 14:56:31 +0100

    Add files via upload

--- features/sample_echo.js
+++ features/sample_echo.js
@@ -26,10 +26,24 @@ module.exports = function (controller) {
             let response = await fetch('https://mp2.gigaaa.link/api/v1/gigaaa-conversations', options)
             let data = await response.json()
             console.log(data);
+            console.log(data.actions.display_list);
             return data;
         }
         getGiga()
-            .then(data => bot.sendMessage(chatId, data.actions.speak.text),
-            );
+            .then(data => {
+                if (data.actions.display_list === undefined) {
+                    bot.sendMessage(chatId, data.actions.speak.text);
+                } else if (data.actions.display_list.elements[0].hasOwnProperty('url')) {
+                    for (let i = 0; i < 4; i++) {
+                        bot.sendMessage(chatId, data.actions.display_list.elements[i].url);
+                    }
+                } else if (data.actions.display_list.elements[0].hasOwnProperty('body') && data.actions.display_list.elements[0].hasOwnProperty('footer')) {
+                    for (let k = 0; k < 3; k++) {
+                        bot.sendMessage(chatId, data.actions.display_list.elements[k].body + " " + data.actions.display_list.elements[k].footer);
+                    }
+                } else {
+                    bot.sendMessage(chatId, data.actions.speak.title);
+                }
+            });
     });
 }
